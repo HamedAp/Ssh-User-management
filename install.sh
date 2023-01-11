29f40c871c6d646f913c288d27f911ee3ae943a0
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-01-12 01:13:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,11 @@ wait
 echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
 wait
 chown www-data:www-data /var/www/html/p/*
+
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+sudo sed -i "/^5829:$port/d" /var/www/html/p/online
+
 elif command -v yum >/dev/null; then
 yum update -y
 yum install httpd php zip net-tools -y
@@ -46,6 +51,11 @@ wait
 echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
 wait
 chown apache:apache /var/www/html/p/*
+
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+sudo sed -i "/^5829:$port/d" /var/www/html/p/online
+
 else
   echo "Centos And Ubunto Supported For Now !!"
 fi
