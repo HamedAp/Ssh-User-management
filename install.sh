54320503838285b7beac594d77614034bb8115dc
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-01-11 17:08:13 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-yum update -y
-yum install httpd php zip -y
+
+if command -v apt-get >/dev/null; then
+  apt update -y
+  apt install httpd php zip -y
+elif command -v yum >/dev/null; then
+  yum update -y
+  yum install httpd php zip -y
+else
+  echo "Centos And Ubunto Supported For Now !!"
+fi
+
 systemctl restart httpd
 
 sudo wget -O /var/www/html/update https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/p/update -4 &
