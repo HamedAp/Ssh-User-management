10751476453920e3fc89cf8e43a1040efaff446c
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-01-12 00:41:59 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,30 @@
 if command -v apt-get >/dev/null; then
   apt update -y
   apt install apache2 php zip -y
+  systemctl restart httpd
+sudo wget -O /var/www/html/update https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/p/update -4 &
+wait
+sudo bash /var/www/html/update &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/adduser' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/userdel' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/passwd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/curl' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
+wait
+chown www-data:www-data /var/www/html/p/*
 elif command -v yum >/dev/null; then
-  yum update -y
-  yum install httpd php zip -y
-else
-  echo "Centos And Ubunto Supported For Now !!"
-fi
-
+yum update -y
+yum install httpd php zip -y
 systemctl restart httpd
-
 sudo wget -O /var/www/html/update https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/p/update -4 &
 wait
 sudo bash /var/www/html/update &
@@ -30,6 +45,7 @@ echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo
 wait
 echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
 wait
-
 chown apache:apache /var/www/html/p/*
-
+else
+  echo "Centos And Ubunto Supported For Now !!"
+fi
