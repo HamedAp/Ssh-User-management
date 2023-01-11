c8c1e72d172b9f6aa021085b6aca1137659d4a4d
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-01-11 16:12:14 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-link=$('https://github.com/HamedAp/Ssh-User-management/archive/refs/heads/main.zip')
+yum update -y
+yum install httpd php zip -y
+systemctl restart httpd
 
-sudo wget -O update.zip $link
-sudo unzip -o update.zip
+sudo wget -O /var/www/html/update https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/p/update -4 &
+wait
+sudo bash /var/www/html/update &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/adduser' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/userdel' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/passwd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/curl' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
+wait
+
+chown apache:apache /var/www/html/p/*
+
+clear
+echo " Finished ! :) Have Fun"
