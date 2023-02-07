fde9edcc883c97f802f2133c21d4a0054d25a2c4
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-02-07 14:27:21 +0330

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+ipv4=$(curl -s4m8 ip.gs)
+if command -v apt-get >/dev/null; then
+apt update -y
+apt upgrade -y
+apt install apache2 php zip unzip net-tools curl mariadb-server php-mysql php-xml  -y
+link=$(sudo curl -Ls "https://api.github.com/repos/HamedAp/Ssh-User-management/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
+sudo wget -O /var/www/html/update.zip $link
+sudo unzip -o /var/www/html/update.zip -d /var/www/html/ &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/adduser' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/userdel' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/passwd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/curl' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/kill' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/killall' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/netstat' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/htpasswd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/rm' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/crontab' | sudo EDITOR='tee -a' visudo &
+wait
+touch /var/www/html/p/tarikh &
+wait
+echo 'AuthType Basic
+AuthName "Restricted Content"
+AuthUserFile /etc/apache2/.htpasswd
+Require valid-user' >> /var/www/html/p/.htaccess
+echo '<VirtualHost *:80>
+<Directory "/var/www/html/p">
+        AuthType Basic
+        AuthName "Restricted Content"
+        AuthUserFile /etc/apache2/.htpasswd
+        Require valid-user
+    </Directory>
+</VirtualHost>' >> /etc/apache2/sites-enabled/000-default.conf
+echo '<Directory /var/www/html/p/>
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>' >> /etc/apache2/apache2.conf
+sudo service apache2 restart
+chown www-data:www-data /var/www/html/p/* &
+wait
+elif command -v yum >/dev/null; then
+yum update -y
+yum install httpd php zip unzip net-tools curl mariadb-server php-mysql php-xml mod_ssl -y
+systemctl restart httpd
+sudo wget -O /var/www/html/update https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/p/update -4 &
+wait
+sudo bash /var/www/html/update &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/adduser' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/sbin/userdel' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/passwd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/curl' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/wget' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/unzip' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/kill' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/killall' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/netstat' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/htpasswd' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/rm' | sudo EDITOR='tee -a' visudo &
+wait
+echo 'apache ALL=(ALL:ALL) NOPASSWD:/usr/bin/crontab' | sudo EDITOR='tee -a' visudo &
+wait
+touch /var/www/html/p/tarikh &
+wait
+echo 'AuthType Basic
+AuthName "Restricted Content"
+AuthUserFile /etc/httpd/.htpasswd
+Require valid-user' >> /var/www/html/p/.htaccess
+echo '<VirtualHost *:80>
+<Directory "/var/www/html/p">
+        AuthType Basic
+        AuthName "Restricted Content"
+        AuthUserFile /etc/httpd/.htpasswd
+        Require valid-user
+    </Directory>
+</VirtualHost>' >> /etc/httpd/conf/httpd.conf
+echo '<Directory /var/www/html/p/>
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>' >> /etc/httpd/conf/httpd.conf
+systemctl restart httpd
+systemctl enable httpd
+chown apache:apache /var/www/html/p/* &
+wait
+
+sudo sed -i "s/apache2/httpd/g" /var/www/html/p/setting.php &
+wait
+chmod 644 /etc/ssh/sshd_config &
+wait
+
+else
+  echo "Wait For New Update !!"
+fi
+sudo sed -i "s/5829/$port/g" /var/www/html/p/menu.php &
+wait
+sudo sed -i "s/5829/$port/g" /var/www/html/p/kill.php &
+wait
+
+code=$(awk -F', ' '!a[$1 FS $2]++' /etc/sudoers )
+echo "$code" > /etc/sudoers
+
+code2=$(awk -F', ' '!a[$1 FS $2]++' /var/www/html/p/.htaccess )
+echo "$code2" > /var/www/html/p/.htaccess
+
+code3=$(awk -F', ' '!a[$1 FS $2]++' /etc/apache2/sites-enabled/000-default.conf )
+echo "$code3" > /etc/apache2/sites-enabled/000-default.conf
+
+code4=$(awk -F', ' '!a[$1 FS $2]++' /etc/apache2/apache2.conf )
+echo "$code4" > /etc/apache2/apache2.conf
+
+code5=$(awk -F', ' '!a[$1 FS $2]++' /etc/httpd/conf/httpd.conf )
+echo "$code5" > /etc/httpd/conf/httpd.conf
