a8eb011e91136ba305a0837fd71079ee5e460a43
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-02-15 15:09:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,5 +173,3 @@ wait
 mysql -e "create database ShaHaN;"
 mysql -e "CREATE USER '${adminusername}'@'localhost' IDENTIFIED BY '${adminpassword}';"
 mysql -e "GRANT ALL ON *.* TO '${adminusername}'@'localhost';"
-
-php /var/www/html/p/restoretarikh.php
