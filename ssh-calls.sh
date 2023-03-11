49325e132753b089670fb36c2d50d563dc6be4f3
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-03-12 01:14:25 +0330

    Create ssh-calls.sh

---
+++ ssh-calls.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/daybreakersx/premscript/master/badvpn-udpgw64"
+sudo touch /etc/rc.local
+
+cat > /etc/rc.local << ENDOFFILE
+#!/bin/sh -e
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 999
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 999
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 999
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 999
+exit 0
+ENDOFFILE
+
+
+
+chmod +x /etc/rc.local
+sudo systemctl status rc-local.service
+sudo chmod +x /usr/bin/badvpn-udpgw
+sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 999
+sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 999
+sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 999
+sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 999
+
+reboot
