c8475e7699358ff355bdfc4d3fe553162b55f82f
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-03-23 00:08:32 +0330

    Delete p directory

--- p/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $port;
-$port = "22";
-$servername = "localhost";
-$username = "adminuser";
-$password = "adminpass";
-$dbname = "ShaHaN";
-date_default_timezone_set("Asia/Tehran");
-?>
