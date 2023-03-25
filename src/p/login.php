e941214718e2e840a856a19316ca7b5a42f78ed9
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-03-25 12:35:45 +0330

    Add files via upload

---
+++ src/p/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>  
+<html lang="en" dir="rtl">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon.svg">
+<title>ShaHaN Panel Login</title>
+<link href="../plugins/bower_components/bootstrap-rtl-master/dist/css/bootstrap-rtl.min.css" rel="stylesheet">
+<link href="css/animate.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/colors/blue.css" id="theme"  rel="stylesheet">
+</head>
+<body>
+<!-- Preloader -->
+<div class="preloader">
+  <div class="cssload-speeding-wheel"></div>
+</div>
+<section id="wrapper" class="login-register" style="background: url(img/login-main.svg) center center/cover no-repeat!important;background-size: 700px 700px !important;">
+  <div class="login-box login-sidebar">
+    <div class="white-box">
+      <form class="form-horizontal form-material" id="loginform" action="login.php">
+        <a href="login.php" class="text-center db"><img src="img/login-small.svg" alt="Home" style="width:50%;"/></a>  
+        
+        <div class="form-group m-t-40">
+          <div class="col-xs-12">
+            <input class="form-control" type="text" required="" placeholder="Username">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-xs-12">
+            <input class="form-control" type="password" required="" placeholder="Password">
+          </div>
+        </div>
+        
+        <div class="form-group text-center m-t-20">
+          <div class="col-xs-12">
+            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit" style="border-radius: 49px;background-color: #006A82 !important;">ورود</button>
+          </div>
+        </div>
+      </form>
+      
+    </div>
+  </div>
+</section>
+<script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../plugins/bower_components/bootstrap-rtl-master/dist/js/bootstrap-rtl.min.js"></script>
+<script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<script src="js/jquery.slimscroll.js"></script>
+<script src="js/waves.js"></script>
+<script src="js/custom.min.js"></script>
+<script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+</body>
+</html>
