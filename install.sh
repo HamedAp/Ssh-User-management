22b04c14f0911d9a438bea3b45b22d89b634057a
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-04-13 16:19:18 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,6 @@ ipv4=$(curl rabin.cf)
 sudo sed -i '/www-data/d' /etc/sudoers
 sudo sed -i '/apache/d' /etc/sudoers
 if command -v apt-get >/dev/null; then
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E5267A6C 
-sudo sysctl net.ipv6.conf.all.disable_ipv6=1
 apt update -y
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php -y
