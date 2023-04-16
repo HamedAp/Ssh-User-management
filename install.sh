93eb5bae7523f3b10a716fe385345d21f087949b
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-04-16 23:40:02 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -213,8 +213,8 @@ cp /var/www/html/p/tarikh /var/www/html/p/backup/tarikh
 rm -fr /var/www/html/p/tarikh
 crontab -l | grep -v '/p/expire.php'  | crontab  -
 crontab -l | grep -v '/p/synctraffic.php'  | crontab  -
-(crontab -l ; echo "* * * * * curl -u $adminusername:$adminpassword http://${ipv4}/p/expire.php >/dev/null 2>&1
-* * * * * curl -u $adminusername:$adminpassword http://${ipv4}/p/synctraffic.php >/dev/null 2>&1" ) | crontab - &
+(crontab -l ; echo "* * * * * curl  http://${ipv4}/p/expire.php >/dev/null 2>&1
+* * * * * curl http://${ipv4}/p/synctraffic.php >/dev/null 2>&1" ) | crontab - &
 wait
 clear
 printf "\nPanel Link : http://${ipv4}/p/index.php"
