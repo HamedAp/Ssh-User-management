552dc0e5ee7e9d44175d9e36efb501fd49df5ccb
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-04-22 01:40:07 +0330

    Update addserver.sh

--- addserver.sh
+++ addserver.sh
@@ -57,7 +57,7 @@ sudo sed -i "s/serverip/$panelip/g" /var/www/html/syncdb.php &
 wait 
 sudo sed -i "s/servertoken/$token/g" /var/www/html/syncdb.php &
 wait 
-chown www-data:www-data /var/www/html/p/* &
+chown www-data:www-data /var/www/html/* &
 wait
 
 echo "* * * * * php /var/www/html/syncdb.php >/dev/null 2>&1" | crontab - &
