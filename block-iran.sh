111d1dfaf6f6707014e2e16bdc4ffb73d9527625
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-04-24 20:50:19 +0330

    Update and rename firwal-conf.sh to block-iran.sh

---
+++ block-iran.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+if [ -z "$port" ]
+then
+sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+pport=$(echo "$po" | sed "s/Port //g")
+fi
+
+
+
+
+apt update -y
+apt install ufw libapache2-mod-geoip geoip-database -y
+a2enmod geoip
+apt install geoip-bin -y
+curl -sSL https://www.ipdeny.com/ipblocks/data/countries/ir.zone | awk '{print "sudo ufw deny out from any to " $1}' | bash
+ufw allow from any to any port $pport proto tcp
+sudo ufw allow 80,$pport,443
+
+ufw enable
+ufw reload
--- firwal-conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt update
-apt install ufw libapache2-mod-geoip geoip-database -y
-a2enmod geoip
-apt install geoip-bin -y
-curl -sSL https://www.ipdeny.com/ipblocks/data/countries/ir.zone | awk '{print "sudo ufw deny out from any to " $1}' | bash
-ufw allow from any to any port 22 proto tcp
-ufw enable
-ufw reload
