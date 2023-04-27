d1f92542d3ea0972e0a9544372852d03972af0ac
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-04-28 02:17:34 +0330

    Update ssh-calls.sh

--- ssh-calls.sh
+++ ssh-calls.sh
@@ -1,46 +1,26 @@
 #!/bin/bash
-
-
-clear
-udpport=7300
-echo -e "\nPlease input UDPGW Port :"
-printf "Default Port is \e[33m${udpport}\e[0m, let it blank to use this Port: "
-read udpport
-
 apt update -y
 apt install git cmake -y
-
-
 git clone https://github.com/ambrop72/badvpn.git /root/badvpn
-
 mkdir /root/badvpn/badvpn-build
-
 cd  /root/badvpn/badvpn-build
-
 cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &
 wait
 make &
 wait
-
 cp udpgw/badvpn-udpgw /usr/local/bin
-
-
 cat >  /etc/systemd/system/videocall.service << ENDOFFILE
 [Unit]
 Description=UDP forwarding for badvpn-tun2socks
 After=nss-lookup.target
 
 [Service]
-ExecStart=/usr/local/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:${udpport} --max-clients 999
+ExecStart=/usr/local/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:7301 --max-clients 999
 User=videocall
 
 [Install]
 WantedBy=multi-user.target
 ENDOFFILE
-
-
 useradd -m videocall
 systemctl enable videocall
 systemctl start videocall
-
-
