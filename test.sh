e9e4144f356e1f9f1b569054b354cdb43a96a714
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-05-07 03:05:39 +0330

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir /var/www/html/p2/
-sudo wget -O /var/www/html/p2/shahan.zip http://shahanpanel.online/shahan.zip
-sudo unzip -o /var/www/html/p2/shahan.zip -d /var/www/html/p2/ &
-wait
