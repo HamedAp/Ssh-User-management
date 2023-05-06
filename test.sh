fa897ac9750ff5c5ee1d925df4fcaa5e1e90d95b
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-05-07 02:29:04 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+mkdir /var/www/html/p2/
+sudo wget -O /var/www/html/p2/shahan.zip http://shahanpanel.online/shahan.zip
+sudo unzip -o /var/www/html/p2/shahan.zip -d /var/www/html/p2/ &
+wait
