4e3aa28b729351c17830b559f9a6a30d0bbb059c
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-06-02 19:23:32 +0330

    Update addserver.sh

--- addserver.sh
+++ addserver.sh
@@ -29,6 +29,8 @@ echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/sed' | sudo EDITOR='tee -a' visud
 wait
 echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/bin/lsof' | sudo EDITOR='tee -a' visudo &
 wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/sbin/lsof' | sudo EDITOR='tee -a' visudo &
+wait
 systemctl restart apache2 &
 wait
 systemctl enable apache2 &
