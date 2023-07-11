fc8de5dbdbca6445b6f304b1991ff99318880878
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-07-11 12:27:37 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,19 @@ if [[ -n "${passwordtmp}" ]]; then
     adminpassword=${passwordtmp}
 fi
 fi
+
+
+file=/etc/systemd/system/videocall.service
+if [ -e "$file" ]; then
+    echo "SSH-CALLS exists"
+else
+udpport=7300
+echo -e "\nPlease input UDPGW Port ."
+printf "Default Port is \e[33m${udpport}\e[0m, let it blank to use this Port: "
+read udpport
+fi
+
+
 ipv4=$(curl -s ipv4.icanhazip.com)
 sudo sed -i '/www-data/d' /etc/sudoers &
 wait
@@ -205,7 +218,31 @@ file=/etc/systemd/system/videocall.service
 if [ -e "$file" ]; then
     echo "SSH-CALLS exists"
 else
-  bash <(curl -Ls https://raw.githubusercontent.com/HamedAp/Ssh-User-management/main/ssh-calls.sh --ipv4)
+apt update -y
+apt install git cmake -y
+git clone https://github.com/ambrop72/badvpn.git /root/badvpn
+mkdir /root/badvpn/badvpn-build
+cd  /root/badvpn/badvpn-build
+cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &
+wait
+make &
+wait
+cp udpgw/badvpn-udpgw /usr/local/bin
+cat >  /etc/systemd/system/videocall.service << ENDOFFILE
+[Unit]
+Description=UDP forwarding for badvpn-tun2socks
+After=nss-lookup.target
+
+[Service]
+ExecStart=/usr/local/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:$udpport --max-clients 999
+User=videocall
+
+[Install]
+WantedBy=multi-user.target
+ENDOFFILE
+useradd -m videocall
+systemctl enable videocall
+systemctl start videocall
 fi
 
 
