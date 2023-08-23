9fd90c44c7f03652b65754b4c79e1b784788b719
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-08-24 01:14:47 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -217,10 +217,12 @@ mysqldump -u ${adminusername} --password=${adminpassword} ShaHaN > /var/www/html
 crontab -l | grep -v '/p/expire.php'  | crontab  -
 crontab -l | grep -v '/p/posttraffic.php'  | crontab  -
 crontab -l | grep -v '/p/synctraffic.php'  | crontab  -
+crontab -l | grep -v '/p/tgexpire.php'  | crontab  -
 crontab -l | grep -v 'p/killusers.sh'  | crontab  -
 crontab -l | grep -v 'p/versioncheck.php'  | crontab  -
 (crontab -l ; echo "1 1-23/2 * * * php /var/www/html/p/versioncheck.php >/dev/null 2>&1
 * * * * * php /var/www/html/p/expire.php >/dev/null 2>&1
+0 0 * * * php /var/www/html/p/tgexpire.php >/dev/null 2>&1
 * * * * * php /var/www/html/p/posttraffic.php >/dev/null 2>&1
 * * * * * bash /var/www/html/p/killusers.sh >/dev/null 2>&1" ) | crontab - &
 wait
