a78fd7bb00555b327150277e1748a68ba9febe2e
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-08-23 11:54:33 +0330

    Update disableipv6.sh

--- disableipv6.sh
+++ disableipv6.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sed -i 's@net.ipv6.conf.all.disable_ipv6 = 1@@' /etc/sysctl.conf
+sed -i 's@net.ipv6.conf.default.disable_ipv6 = 1@@' /etc/sysctl.conf
 
 echo "net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
