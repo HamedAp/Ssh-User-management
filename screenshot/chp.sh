839298877adbceb34605a95d038773a2389bfc52
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-08-31 13:33:01 +0330

    Update chp.sh

--- screenshot/chp.sh
+++ screenshot/chp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
 ipv4=$(curl -s ipv4.icanhazip.com)
-panelPort=80
+panelPortt=80
 echo -e "\nPlease input Panel Port ."
 printf "Default Port is \e[33m${panelPort}\e[0m, let it blank to use this Port: "
-read panelPort
-   echo "<VirtualHost 0.0.0.0:$panelPort>
+read panelPortt
+   echo "<VirtualHost 0.0.0.0:$panelPortt>
         ServerAdmin ShaHaN@localhost
         DocumentRoot /var/www/html/
         ErrorLog ${APACHE_LOG_DIR}/error.log
@@ -15,7 +15,7 @@ read panelPort
         </Directory>
     </VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 
-   echo "Listen 0.0.0.0:$panelPort
+   echo "Listen 0.0.0.0:$panelPortt
 <IfModule ssl_module>
         Listen 443
 </IfModule>
@@ -25,10 +25,10 @@ read panelPort
 </IfModule>" > /etc/apache2/ports.conf
 
 
-ufw allow $panelPort/tcp
+ufw allow $panelPortt/tcp
 systemctl restart apache2 
 
 clear 
-echo -e "\nPanel Port Changed To : $panelport"
-echo -e "\nPanel Address : http://$ipv4:$panelport/p/index.php"
+echo -e "\nPanel Port Changed To : $panelPortt"
+echo -e "\nPanel Address : http://$ipv4:$panelPortt/p/index.php"
 echo -e "\n"
