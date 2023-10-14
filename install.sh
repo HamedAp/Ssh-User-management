561ac66f27f9025090706df1203423383df57bee
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-10-14 23:55:59 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -289,10 +289,7 @@ fi
 if ! grep -q "Match group jailed" /etc/ssh/sshd_config
 then
   echo "Users Limited From SSH Login"
-  echo "
-Match group jailed
-  ChrootDirectory $JAILPATH
-" >> /etc/ssh/sshd_config
+
 fi
 sudo chmod 400 /jailed/
 systemctl restart sshd
