1e8f7f4dc5d31251c48906b3496504aac8148e2b
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-10-27 23:42:20 +0330

    Create fakewebsite.sh

---
+++ fakewebsite.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# By Hamed Ap
+# For Shahan Panel :) Dozdi Nakon BiShoor :))
+
+apt install unzip -y
+cd /root
+if [[ -d "randomfakehtml-master" ]]; then
+	cd randomfakehtml-master
+else
+	wget https://github.com/GFW4Fun/randomfakehtml/archive/refs/heads/master.zip
+	unzip master.zip && rm master.zip
+	cd randomfakehtml-master
+	rm -rf assets
+	rm ".gitattributes" "README.md" "_config.yml"
+fi
+
+RandomHTML=$(a=(*); echo ${a[$((RANDOM % ${#a[@]}))]} 2>&1)
+msg_inf "Random template name: ${RandomHTML}"
+
+if [[ -d "${RandomHTML}" && -d "/var/www/html/" ]]; then
+	cp -a ${RandomHTML}/. "/var/www/html/"
+ clear
+	msg_ok "Fake Website Installed ! :) "
+else
+	msg_err "Extraction error!"
+fi
