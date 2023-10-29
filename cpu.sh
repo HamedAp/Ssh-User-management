c4ec59f3d5a5a4a828850688616d98f3e1058b13
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-10-30 01:15:25 +0330

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+printshahan() {
+    text="$1"
+    delay="$2"
+    for ((i=0; i<${#text}; i++)); do
+        echo -n "${text:$i:1}"
+        sleep $delay
+    done
+    echo
+}
 clear
 echo ""
 printshahan "This Script Will Reboot Server" 0.1
