906091c72e99d4428ac6e47db13c309176087b78
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-11-21 02:30:45 +0330

    Update token.php

--- New-Server/token.php
+++ New-Server/token.php
@@ -1,46 +1,4 @@
-<?php //ICB0 81:0 82:84a                                                      ?><?php //004a4
-// 
-//    _____ _           _    _       _   _
-//   / ____| |         | |  | |     | \ | |
-//  | (___ | |__   __ _| |__| | __ _|  \| |
-//   \___ \| '_ \ / _` |  __  |/ _` | . ` |
-//   ____) | | | | (_| | |  | | (_| | |\  |
-//  |_____/|_| |_|\__,_|_|  |_|\__,_|_| \_|
-// 
-// 
-//   _____      _   _      _
-//  |  __ \    | \ | |    | |
-//  | |__) |_ _|  \| | ___| |
-//  |  ___/ _` | . ` |/ _ \ |
-//  | |  | (_| | |\  |  __/ |____
-//  |_|   \__,_|_| \_|\___|______|
-// 
-// 
-if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+<?php
+$ip = "serverip";
+$token = "servertoken";
 ?>
-HR+cPtiNqSQzvYdfqgn4vIb6eKyrCG1ZK2+C3BhI5sX3KDSkFYsarovMiG4MiJCcMS41/YH6poDO
-P6MWF/qGfzc9wAgD272nshDG7QHx8vfdLIq61tEZGtY22v7OETkn4N7tamH1j/eEbmLzSPaGL6ci
-zwBkRugmI4fh4kbwd6UuAJQkADBTcSUb4VVcW7QyBdKJe/f5IYSdpyPRHjVGh2YkDSIUbgopOGAU
-52SQX5n7iAEihWqMWqdPrHpGnxr6oQ63Fy9NZq/zpnvVP+vNac0X+2J11b2B9EELE4D3W9KSIcnk
-VUYf+L3bWDDPtc7Aad2wXKwCyYjjMW9fUhMfPnl/vopeUxeTKBznXPsfZVWWG8/OJVkBUrOJnqx0
-37sSk1sh2DLRqTvEBLBiODbyWbH46c+p20ugYhyLBJPr19BAkpCd0jxfMSREMju2qWeBWTj5YmV4
-mWdwNK7x0VNZ0DUseEe/ikZbXUhK766FwES6Du1p3mtoV9AejYQuvUm8ydzr1N4uyFQLkvvRXNwQ
-z32bW7bXBHHVO0tCWzmhop3ixvWlzLLQECZDrSl/yL42z3kOto50ng3abf/RuPiARJxnuYYmhMFi
-mhMgptMs8tiTm14I4hVuk2k6QchNSXia+fZetLER5I0CtM3oHVUdwpikzljA3i6XUpYhGjZfH+RV
-QGdiACBADsTZfbI9VpcPfmJduZach/nxan0k0SR1Fp83Fa3mchJJ760hktdx4rnAZf0EwbmXbbXS
-gfavwyc3C34JARWUVsZH6V/QIGrsHkyJzz2P5g0SGOqN9ztNjJTHOu8kkMMeR8OC0HpFDduvLR/V
-PTX0Kk3IlCao646H1KFD58G4cHbcgMkvYVyiU9tyJRoaPTBg4uQ/7SCBNavnFH1QqICe0Im1lslH
-ENa==
-HR+cPpuZIT+6yB4GDPtS/4EYzDgb24uoDnd/3UmUe4els55fd60rm853FbOiKI0G7I3T8Jk0S3T7
-BNT5qXtHJON1udE6oRHXHX+ffiApKTezwlTj4DxzTaPLFO2LWJ7MAnhgK8snMrdovy3z7dJ1aczW
-ydxM0xgtfbt5eJ5jnNk1j1MZPtTmQh0tBmmDmX9l3QMJu/NyaJirSuCuUIOP5LQBDrlqjBWKxa22
-0n0oQEEqhYliQlSL894hxZvMOu0UYFvSxes1QutjNnmAwzeMP1AGySKP1wv27WnRuaZgItwjGBqb
-h+Qh36SGDcaKJcw6JSz1RqjqHmwSlmecx4lRxF67Il+AQiYuKTLdeh2U4ETB6WsyH04Hoa5sAAZW
-lCD/6EDaH8WHiJ2jSj36YwowPB2GuwhUr7IHd7LeCOWlk9yd69i33iZ0OrS6YuHturaqNs9ZZghB
-bXNh4TLyP0eETp8tO5qSmmPnSAKD6RnmeuCEHAS7qHBJKZ19283YYWGrCOUESKKnfQEy2dazVv3e
-bsdWUFkbIior2KHY8uxloeP82iWDmDFiL+jtmFs8ZBFRE3+sPonH+sx5WjbyNOdeB8tAvdRRnQNI
-jN9vQlFZYzmIxb9e9G0sq/FMw9lR6AA2iADwEDICC2eZXbQVli7i+WNfAMNzoy+FhZUvcPIuWR3V
-yx0B2uYmRZX2GyVpKurpYvzVIFGllxvD+6vFw3fwd2x0b0KStPMsYB5EvAWfGlZQW1YncHfB75Z1
-DehwiN51+n72n+uMdha+OivFeQ6VoR5j4YG+2H7NINlctv+tLq/GukRhFMD0Yws0bkZECuRmm2zB
-yy4zvs4SS3bmdLocvzTT7WYELyv4DtOwn2GaBtHbefNurVmksiSn2OZEcC8j5MYMB/7fAVKwP6Ur
-xEwmkddRmrm=
