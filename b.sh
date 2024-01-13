cf8c1d63cef9c1d2284dad598a8c5e44ed889ed4
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-01-13 22:07:36 +0330

    Update b.sh

--- b.sh
+++ b.sh
@@ -14,6 +14,7 @@ ipset create whitelist hash:net
 while read line; do ipset add whitelist $line; done < /root/iranip.txt
 iptables -A INPUT -p tcp --dport 22 -m set --match-set whitelist src -j ACCEPT
 iptables -A INPUT -p tcp --dport $port -m set --match-set whitelist src -j ACCEPT
+iptables -A INPUT -p tcp -m set --match-set whitelist src -j ACCEPT
 iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
 iptables -A OUTPUT -m set --match-set whitelist src -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
