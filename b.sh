6cc58e142752d054ef8614ceafee966b5089361e
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-01-13 21:29:39 +0330

    Update b.sh

--- b.sh
+++ b.sh
@@ -9,10 +9,11 @@ sudo wget -4 -O /root/iranip.txt https://raw.githubusercontent.com/HamedAp/Ssh-U
 wait
 
 iptables -F
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport $port -j ACCEPT
+
 ipset create whitelist hash:net
 while read line; do ipset add whitelist $line; done < /root/iranip.txt
+iptables -A INPUT -p tcp --dport 22 -m set --match-set whitelist src -j ACCEPT
+iptables -A INPUT -p tcp --dport $port -m set --match-set whitelist src -j ACCEPT
 iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -I INPUT 1 -i lo -j ACCEPT
