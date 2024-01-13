672dd328908784248a48b7e6c96b775616f75b96
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-01-14 00:11:00 +0330

    Update b.sh

--- b.sh
+++ b.sh
@@ -12,13 +12,13 @@ iptables -F
 
 ipset create whitelist hash:net
 while read line; do ipset add whitelist $line; done < /root/iranip.txt
-iptables -A INPUT -p tcp --dport 22 -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -p tcp --dport $port -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -p tcp -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
+#iptables -A INPUT -p tcp --dport 22 -m set --match-set whitelist src -j ACCEPT
+#iptables -A INPUT -p tcp --dport $port -m set --match-set whitelist src -j ACCEPT
+#iptables -A INPUT -p tcp -m set --match-set whitelist src -j ACCEPT
+#iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
 iptables -A OUTPUT -m set --match-set whitelist src -j DROP
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -I INPUT 1 -i lo -j ACCEPT
-iptables -A INPUT -j DROP
+#iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+#iptables -I INPUT 1 -i lo -j ACCEPT
+#iptables -A INPUT -j DROP
 clear 
-echo "Blocked All Country Incomming Except Iran :) "
+echo "Blocked Iran Ip :)"
