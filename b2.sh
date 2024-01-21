5c8c3ad94ea196168d5920f0b2b2bbac6685f067
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-01-22 02:08:25 +0330

    Update b2.sh

--- b2.sh
+++ b2.sh
@@ -21,5 +21,5 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -I INPUT 1 -i lo -j ACCEPT
 iptables -A INPUT -j DROP
 clear 
-echo "Blocked Iran Ip :)\n"
+echo "Blocked Iran Ip :)"
 echo "Blocked Panel From Outside :)"
