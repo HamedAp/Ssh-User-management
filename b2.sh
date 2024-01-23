222def7ae37c1e2aff78c8ffb213d3579857c5a2
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-01-23 21:09:06 +0330

    Update b2.sh

--- b2.sh
+++ b2.sh
@@ -20,6 +20,7 @@ iptables -A OUTPUT -m set --match-set whitelist src -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -I INPUT 1 -i lo -j ACCEPT
 iptables -A INPUT -j DROP
+sudo iptables-save
 clear 
 echo "Blocked Iran Ip :)"
 echo "Blocked Panel From Outside :)"
