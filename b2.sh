e644605ca15cf8de226a387bef58f9806d7e1add
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-01-28 19:26:42 +0330

    Update b2.sh

--- b2.sh
+++ b2.sh
@@ -10,13 +10,13 @@ wait
 
 iptables -F
 
-ipset create whitelist hash:net
-while read line; do ipset add whitelist $line; done < /root/iranip.txt
-iptables -A INPUT -p tcp --dport 22 -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -p tcp --dport $port -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -p tcp -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
-iptables -A OUTPUT -m set --match-set whitelist src -j DROP
+ipset create shahaniran hash:net
+while read line; do ipset add shahaniran $line; done < /root/iranip.txt
+iptables -A INPUT -p tcp --dport 22 -m set --match-set shahaniran src -j ACCEPT
+iptables -A INPUT -p tcp --dport $port -m set --match-set shahaniran src -j ACCEPT
+iptables -A INPUT -p tcp -m set --match-set shahaniran src -j ACCEPT
+iptables -A INPUT -m set --match-set shahaniran src -j ACCEPT
+iptables -A OUTPUT -m set --match-set shahaniran src -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -I INPUT 1 -i lo -j ACCEPT
 iptables -A INPUT -j DROP
