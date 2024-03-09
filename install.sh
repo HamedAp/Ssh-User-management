f9fb5ced165db0c208cb3d827ed3a0ca7cb7b300
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-03-10 00:48:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -111,6 +111,8 @@ sed -i 's@#PrintMotd no@PrintMotd yes@' /etc/ssh/sshd_config
 
 
 if command -v apt-get >/dev/null; then
+echo "#shahanDNS
+nameserver 8.8.8.8" > /etc/resolv.conf
 apt update -y
 apt upgrade -y
 rm -fr /etc/php/7.4/apache2/conf.d/00-ioncube.ini
