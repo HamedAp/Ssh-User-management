030c5793043980a816c78c97f4b89c18a43fde38
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2025-05-30 12:03:25 +0330

    Create fixshadow.sh

---
+++ fixshadow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat >  /root/fixshadowsocks.sh << ENDOFFILE
+#!/bin/bash
+
+if systemctl is-active --quiet shadowsocks; then
+    echo "shadowsocks is already running"
+else
+    if sudo systemctl start shadowsocks; then
+        echo "shadowsocks started successfully"
+    else
+        echo "Failed to start shadowsocks"
+        exit 1
+    fi
+fi
+
+ENDOFFILE
+sudo chmod a+rx /root/fixshadowsocks.sh
+
+
+(sudo crontab -l ; echo "* * * * * bash /root/fixshadowsocks.sh >/dev/null 2>&1") | crontab -
+
+clear
+echo "Shadowsocks Fixer Installed ."
