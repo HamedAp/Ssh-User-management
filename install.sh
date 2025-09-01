f7a2878f734f133b0afc0560ca659a0e5dbb1817
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2025-09-02 01:55:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -295,6 +295,8 @@ echo 'www-data ALL=(ALL:ALL) NOPASSWD:/bin/ovpm' | sudo EDITOR='tee -a' visudo &
 wait
 echo 'www-data ALL=(ALL:ALL) NOPASSWD:/bin/systemctl restart shahanguard' | sudo EDITOR='tee -a' visudo &
 wait
+echo 'www-data ALL=(ALL:ALL) NOPASSWD:/usr/local/bin/shahanguard' | sudo EDITOR='tee -a' visudo &
+wait
 sudo sed -i '/%sudo/s/^/#/' /etc/sudoers &
 wait
 echo "application/json      json" >> /etc/mime.types
